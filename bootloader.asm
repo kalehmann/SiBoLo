b124cc6c87cf9d6253a796e1ef071d48eb9c3a9e
Karsten Lehmann <ka.lehmann@yahoo.com>
2017-07-19 11:34:28 +0200

    Update bootloader.asm

--- bootloader.asm
+++ bootloader.asm
@@ -396,6 +396,6 @@ cmp_f_names:
 					; bootloader.
 
 ; The bios loads exact 512 bytes. Here we fill this file to byte 510 with zeros
-; and the add a "boot signature", whatever this shit is.
+; and the add the boot signature.
 	times 510-($-$$) db 0	; Pad remainder of boot sector with 0s
 	dw 0xAA55		; The standard PC boot signature
